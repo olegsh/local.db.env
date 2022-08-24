4b6b34ead606cea05616924cf7bdc288c270b02d
Oleg S <tanoleg@gmail.com>
2022-08-24 09:47:50 -0700

    SQL questions, interview preps

---
+++ leetcode/CustomerNeverOrder.sql
@@ -0,0 +1,36 @@
+/*
+183. Customers who never ordered
+https://leetcode.com/problems/customers-who-never-order/
+
+Table: Customers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID and name of a customer.
+
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key column for this table.
+customerId is a foreign key of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+
+Write an SQL query to report all customers who never order anything.
+Return the result table in any order.
+The query result format is in the following example.
+*/
+
+select c.name as 'Customers' from Customers c
+where c.id not in (
+    select o.customerId from Orders o
+);
